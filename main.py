2ba1ae3b58f0345070b06e7d88747826e3d89f58
Ryan Adrian <NayrAdrian24@gmail.com>
2024-06-11 09:40:10 +0800

    added question and answer function, scoring and show answer in map function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
@@ -6,5 +7,23 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
+# Read data from CSV and convert states to lowercase
+all_states = data["state"].tolist()
+guessed_states = []
 
-answer_state = screen.textinput(title="Guess the State",prompt="What's another state's name?")
+while len(guessed_states) < 50:
+    answer_state = (screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                     prompt="What's another state's name?")).title()
+
+    # Check if the answer is in the list of states
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
+        t.write(answer_state)
+
+screen.exitonclick()
